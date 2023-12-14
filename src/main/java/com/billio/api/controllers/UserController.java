b3b258532d370aab29996fc6ea6d9c1c39d289e2
sunwoojeong <sunwoojeongcoding@gmail.com>
2023-12-13 19:07:35 -0800

    git_initial_api_mysql_hibernate_creat_random_user

---
+++ src/main/java/com/billio/api/controllers/UserController.java
@@ -0,0 +1,21 @@
+package com.billio.api.controllers;
+
+import com.billio.api.entities.User;
+import com.billio.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/create-random")
+    public User createRandomUser() {
+        return userService.createRandomUser();
+    }
+}
---
+++ src/main/java/com/billio/api/entities/User.java
@@ -0,0 +1,31 @@
+package com.billio.api.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Entity
+@Table(name = "user") // Use backticks if you want to use "user" as the table name
+@Getter @Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+
+    // other fields, getters, and setters
+}
